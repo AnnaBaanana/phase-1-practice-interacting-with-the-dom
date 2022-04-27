3746c2c372f19a1d73ea486d368e2bb404cb3d61
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-26 21:07:35 -0400

    adding counter, manual counter increase, and liker functionality

---
+++ js/challenge.js
@@ -0,0 +1,63 @@
+let intTime;
+const counter = document.getElementById('counter')
+const minus = document.getElementById('minus')
+const plus = document.getElementById('plus')
+const heart = document.getElementById('heart')
+const pause = document.getElementById('pause')
+
+function timer() {
+    intTime = setInterval(() => counter.textContent++, 1000)}
+
+function decreaseTimer() {
+    minus.addEventListener('click', ()=> {
+        counter.textContent--
+    })
+}
+
+function increaseTimer() {
+    plus.addEventListener('click', ()=> {
+        counter.textContent++
+    })
+}
+
+function liker() {
+    //const numArray = [];
+    //const countLikes = {};
+    heart.addEventListener('click', ()=> {
+        const likes = document.querySelector('.likes')
+        console.log(likes)
+        const li = document.createElement('li')
+        li.textContent = `${heart.textContent} ${counter.textContent}`
+        console.log(li)
+        likes.append(li)
+    })
+}
+
+function pauser() {
+    pause.addEventListener('click', (e)=> {
+        e.preventDefault()
+        console.log(e)
+        console.log(e.target)
+        if (e.target.outerText==='pause') {
+            e.target.outerText='resume'
+        } else {e.target.outerText='pause'}
+    })
+}
+
+function stopTimer() {
+    let currentTime = counter.textContent
+    counter.TextContent=currentTime
+}
+
+const domLoaded = () => {
+    document.addEventListener('DOMContentLoaded', 
+    () => {console.log('DOM Loaded')
+    timer(),
+    decreaseTimer(),
+    increaseTimer(),
+    liker(),
+    pauser()
+})
+}
+
+domLoaded()
